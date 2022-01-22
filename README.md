152829c6f2c8a8aebd21a442854e66a07b32f3ac
NathanM <nathan.milburn@outlook.com>
2022-01-22 15:59:50 -0800

    redid my table formatting based on recommendations from W3Schools forums. Added a To Do list for my javascript page. Made additional updates to my README.md file including my User Story and links to deployed application and repository

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 MY USER STORY
 
-AS A 
+AS AN Aspiring web developer and software engineer
 
-I WANT 
+I WANT to create a daily schedule planner that accounts for the current timestamp and displays what time slots are available or not for past, current, and future appointments
 
-SO THAT 
+SO THAT I can showcase my javascript, bootstrap, moment and jquery fundamentals while also having a new schedule planner that I can use daily (and be proud of!)
 
 SUPPLIED USER STORY
 
@@ -15,3 +15,6 @@ AS AN employee with a busy schedule
 I WANT to add important events to a daily planner
 
 SO THAT I can manage my time effectively
+
+[DeployedSchedulePlannerApplication](https://nathanmilburn.github.io/NBM-Homework-05-Work-Day-Scheduler/)
+[DeployedSchedulePlannerRepository](https://github.com/NathanMilburn/NBM-Homework-05-Work-Day-Scheduler)
--- script.js
+++ script.js
@@ -2,5 +2,22 @@ var currentDate = moment().format('dddd MMMM Do, YYYY');
 
 $("#currentDay").text(currentDate);
 
-var currentHour = moment().format('h:mm:ss a');
-$("#currentHour").text(currentHour);
+// Placeholder
+
+var timeDisplayEl = $('#currentHour');
+function displayTime() {
+    var currentHour = moment().format('h:mm:ss a');
+    timeDisplayEl.text(currentHour);
+}
+
+setInterval(displayTime, 1000);
+
+// TO DO LIST:
+
+// Attach moment().format functions to timeslot fields to change colors based on current timestamp
+// Potentially use an IF statement for color changes based on current timestamp
+// Maybe replace form field with input fields in the appointment section of the table
+// Connect the save button with a localstorage save
+// IF I use a form field, incorporate a preventDefault to stop the page from reloading each time
+// Maybe use a fourth color option for appointments that have been committed (maybe yello?)
+// Learn more ways I can incorporate jquery UI and Bootstrap to do some of the work for me.
