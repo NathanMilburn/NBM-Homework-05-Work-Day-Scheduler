129ffc93c3affe6d74b50c9be575e56e8d34ce82
NathanM <nathan.milburn@outlook.com>
2022-01-27 18:34:46 -0800

    added id tags to each time-block element for a future for loop to iterate each one

--- script.js
+++ script.js
@@ -6,15 +6,27 @@ var past = $('.past');
 var present = $('.present');
 var future = $('.future');
 
-const timeBlock9 = document.getElementById('#9')
-const timeBlock10 = document.getElementById('#10')
-const timeBlock11 = document.getElementById('#11')
-const timeBlock12 = document.getElementById('#12')
-const timeBlock13 = document.getElementById('#13')
-const timeBlock14 = document.getElementById('#14')
-const timeBlock15 = document.getElementById('#15')
-const timeBlock16 = document.getElementById('#16')
-const timeBlock17 = document.getElementById('#17')
+
+// const timeBlock9 = document.getElementById('#9')
+// const timeBlock10 = document.getElementById('#10')
+// const timeBlock11 = document.getElementById('#11')
+// const timeBlock12 = document.getElementById('#12')
+// const timeBlock13 = document.getElementById('#13')
+// const timeBlock14 = document.getElementById('#14')
+// const timeBlock15 = document.getElementById('#15')
+// const timeBlock16 = document.getElementById('#16')
+// const timeBlock17 = document.getElementById('#17')
+
+// Timeblock variables for regular business hours (used increments from military time format)
+const timeBlock9 = $('#9');
+const timeBlock10 = $('#10');
+const timeBlock11 = $('#11');
+const timeBlock12 = $('#12');
+const timeBlock13 = $('#13');
+const timeBlock14 = $('#14');
+const timeBlock15 = $('#15');
+const timeBlock16 = $('#16');
+const timeBlock17 = $('#17');
 
 
 var currentDate = moment().format('dddd MMMM Do, YYYY');
@@ -28,21 +40,6 @@ function displayTime() {
     timeDisplayEl.text(currentHour);
 }
 
-// setInterval(displayTime, 1000);
-
-// saveBtn function #1?
-// var lockAppointmentSlot = function (event) {
-//     event.preventDefault();
-
-//     var appointmentInput = inputAppointment.value.trim();
-
-//     if (appointmentInput) {
-
-//     }
-
-//     localStorage.setItem('textarea', JSON.stringify(appointmentInput))
-//     window.location.assign('.time-block')
-// }
 
 // styling time-blocks for hours that have alread passed
 function pastTimeSlot() {
@@ -55,40 +52,52 @@ function pastTimeSlot() {
 
 // styling time-blocks for the current hour
 function currentTimeSlot() {
-
+    for(var i = currentHour; i >= 9; i){
+        if(i <= 9 && i <= 17){
+            document.getElementById(i).classList.add('present');
+        }
+    }
 }
 
 // styling time-blocks for hours that haven't alread passed
 function futureTimeSlot() {
-    for(var i = currentHour + 1; i >= 9; i++){
-        var temp = document.getElementById('.text-block')
-        element.classList.add('.future');
+    for(var i = currentHour + 1; i <= 17; i++){
+        if(i <= 17){
+            document.getElementById(i).classList.add('future');
+        }
     }
 }
 
 pastTimeSlot()
+
+futureTimeSlot()
 displayTime()
 
-// TO DO LIST:
-
-// Attach moment().format functions to timeslot fields to change colors based on current timestamp
-// Potentially use an IF statement for color changes based on current timestamp
-// Maybe replace form field with input fields in the appointment section of the table
-// Connect the save button with a localstorage save
-// Maybe use a fourth color option for appointments that have been committed (maybe yello?)
-// Learn more ways I can incorporate jquery UI and Bootstrap to do some of the work for me.
-
 var saveBtn = $('.saveBtn');
-var inputText = $('textarea[name=input-box]');
+var inputText = $('textarea');
 
+// Accepting Input Function
 function getInput() {
     const inputField = {
         inputText: inputText.val()
     };
 
-    localStorage.setItem('textarea', JSON.stringify(inputField))
+    localStorage.getItem('textarea', JSON.stringify(inputField))
 };
+// Test for loop for get input 
+// var table = $('textarea[id]')
+// function getInput() {
+//     for(var i = 0; row; row = table.rows[i]; i++){
+//         if()
+//     }
+//     const inputField = {
+//         inputText: inputText.val()
+//     };
 
+//     localStorage.getItem('textarea', JSON.stringify(inputField))
+// };
+
+// Saving Input Function
 function saveInput() {
     inputValue = JSON.parse(localStorage.getItem('textarea'));
 
@@ -111,3 +120,12 @@ function maintain() {
 }
 
 maintain();
+
+// TO DO LIST:
+
+// Attach moment().format functions to timeslot fields to change colors based on current timestamp
+// Potentially use an IF statement for color changes based on current timestamp
+// Maybe replace form field with input fields in the appointment section of the table
+// Connect the save button with a localstorage save
+// Maybe use a fourth color option for appointments that have been committed (maybe yello?)
+// Learn more ways I can incorporate jquery UI and Bootstrap to do some of the work for me.
